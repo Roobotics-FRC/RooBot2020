fc64a40cb26097dca59ee3900a88f3fbdd28db1e
aaplmath <aaplmath@users.noreply.github.com>
2020-01-24 13:07:29 -0500

    Add motor configuration utility classes to RobotMap

--- src/main/java/frc/team4373/robot/RobotMap.java
+++ src/main/java/frc/team4373/robot/RobotMap.java
@@ -1,5 +1,7 @@
 package frc.team4373.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
 /**
  * Holds various mappings and constants.
  */
@@ -9,4 +11,66 @@ public class RobotMap {
     public static final int DRIVE_JOYSTICK_PORT = 0;
     public static final int OPERATOR_JOYSTICK_PORT = 1;
     public static final double JOYSTICK_DEFAULT_DEADZONE = 0.09;
+
+    // Utility classes
+    public static final class MotorConfig {
+        public final int id;
+        public final boolean inverted;
+        public final NeutralMode neutralMode;
+        public final boolean encoderPhase;
+        public final PID gains;
+
+        /**
+         * Constructs a new MotorConfig for a motor using closed-loop control.
+         * @param id the CAN ID of the motor.
+         * @param inverted whether to invert motor output values.
+         * @param neutralMode the motor's neutral mode.
+         */
+        public MotorConfig(int id, boolean inverted,
+                           NeutralMode neutralMode, boolean encoderPhase, PID gains) {
+            this.id = id;
+            this.inverted = inverted;
+            this.neutralMode = neutralMode;
+            this.encoderPhase = encoderPhase;
+            this.gains = gains;
+        }
+
+        /**
+         * Constructs a new MotorConfig for a motor not under closed-loop control.
+         * @param id the CAN ID of the motor.
+         * @param inverted whether to invert motor output values.
+         * @param neutralMode the motor's neutral mode.
+         */
+        public MotorConfig(int id, boolean inverted, NeutralMode neutralMode) {
+            this.id = id;
+            this.inverted = inverted;
+            this.neutralMode = neutralMode;
+
+            this.encoderPhase = false;
+            this.gains = new PID(0, 0, 0,0);
+        }
+
+    }
+
+    // PID- and sensor-related constants
+    public static final class PID {
+        public final double kF;
+        public final double kP;
+        public final double kI;
+        public final double kD;
+
+        /**
+         * Constructs a new PID parameters object.
+         * @param kF feed-forward gain.
+         * @param kP proportional gain.
+         * @param kI integral gain.
+         * @param kD derivative gain.
+         */
+        public PID(double kF, double kP, double kI, double kD) {
+            this.kF = kF;
+            this.kP = kP;
+            this.kI = kI;
+            this.kD = kD;
+        }
+    }
 }
