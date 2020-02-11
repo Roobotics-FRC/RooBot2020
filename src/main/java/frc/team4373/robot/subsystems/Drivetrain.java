6c0bb1edc6b628c78011df3ace68b78e7e28fd9f
aaplmath <aaplmath@users.noreply.github.com>
2020-02-11 15:51:57 -0500

    Fix joystick y-axis

--- src/main/java/frc/team4373/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team4373/robot/subsystems/Drivetrain.java
@@ -33,8 +33,8 @@ public class Drivetrain extends SwerveDrivetrain {
     @Override
     protected void initDefaultCommand() {
         setDefaultCommand(new SwerveDriveWithJoystick(
-                OI.getInstance().getDriveJoystick()::rooGetX,
-                OI.getInstance().getDriveJoystick()::rooGetY,
+                OI.getInstance().getDriveJoystick()::rooGetX, () ->
+                -OI.getInstance().getDriveJoystick().rooGetY(),
                 OI.getInstance().getDriveJoystick()::rooGetTwist,
                 this));
     }
