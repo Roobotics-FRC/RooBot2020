caacf58da50f701137c08d713db2913492395f26
Aria <23ariag@abingtonfriends.net>
2020-02-14 17:15:06 -0500

    Create DriveAndShootAuton

---
+++ src/main/java/frc/team4373/robot/commands/DriveAndShootAuton.java
@@ -0,0 +1,15 @@
+package frc.team4373.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class DriveAndShootAuton extends CommandGroup {
+
+    /**
+     * Drives off the initiation line, turns to a specified angle, and  shoots.
+     */
+    public DriveAndShootAuton() {
+        addSequential(new TimedDriveAuton(0.5, 1, 0));
+        addSequential(new VisionQuerierCommand("angle_offset", 2, RotateAngleOffsetAuton::new));
+        addSequential(new ShooterShootCommand(),5);
+    }
+}
