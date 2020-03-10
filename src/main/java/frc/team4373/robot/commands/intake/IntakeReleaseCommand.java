500466c60d2509de6d55e64c7d9cc4439fa77d57
aaplmath <aaplmath@users.noreply.github.com>
2020-03-10 16:49:40 -0400

    Make intake release auton wait before release

--- src/main/java/frc/team4373/robot/commands/intake/IntakeReleaseCommand.java
+++ src/main/java/frc/team4373/robot/commands/intake/IntakeReleaseCommand.java
@@ -4,19 +4,28 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.team4373.robot.subsystems.Intake;
 
 public class IntakeReleaseCommand extends Command {
+    private static double WAIT_ITERATIONS = 25;
     private Intake intake;
+    private double iterations = 0;
 
     public IntakeReleaseCommand() {
         requires(this.intake = Intake.getInstance());
     }
 
     @Override
+    protected void initialize() {
+        this.iterations = 0;
+    }
+
+    @Override
     protected void execute() {
-        this.intake.releaseBall();
+        if (++this.iterations >= WAIT_ITERATIONS) {
+            this.intake.releaseBall();
+        }
     }
 
     @Override
     protected boolean isFinished() {
-        return false;
+        return this.iterations > WAIT_ITERATIONS;
     }
 }
